846e5e36e07c5d17e677d1a1f0b1d8871c1a7c97
sivakusimk6 <sivakusi12@gmail.com>
2018-01-28 23:09:40 +0530

    nav fixed

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 'use strict';
 $(function () {
 
-	$(".dd").on('click', function() {
+	$("#navigation .dd").on('click', function() {
 		$(('#'+id).show).removeClass('show');
 		var id= $(this).attr('data-list');
 		$('#'+id).slideToggle(500);
 		$('#'+id).removeClass('hidden');
 		$('#'+id).addClass('show');
 	});
-	$(".dd").on('blur', function() {
+	$("#navigation .dd").on('blur', function() {
 		var id= $(this).attr('data-list');
 		$('#'+id).slideToggle(500);
 		$('#'+id).removeClass('show');
 		$('#'+id).addClass('hidden');
+	});
+
+	$("#menu-icon").on('click', function() {
+		$('#main-list').toggleClass('show');
+		$('#main-list').slideToggle(300);
 	});
 });
