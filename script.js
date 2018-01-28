c108a039df6daf0d1d66933325fcd68007042f29
sivakusimk6 <sivakusi12@gmail.com>
2018-01-28 23:40:56 +0530

    modified nav

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ $(function () {
 		$(('#'+id).show).removeClass('show');
 		var id= $(this).attr('data-list');
 		$('#'+id).slideToggle(500);
-		$('#'+id).removeClass('hidden');
-		$('#'+id).addClass('show');
+		// $('#'+id).removeClass('hidden');
+		// $('#'+id).addClass('show');
 	});
 	$("#navigation .dd").on('blur', function() {
 		var id= $(this).attr('data-list');
 		$('#'+id).slideToggle(500);
-		$('#'+id).removeClass('show');
-		$('#'+id).addClass('hidden');
+		// $('#'+id).removeClass('show');
+		// $('#'+id).addClass('hidden');
 	});
 
 	$("#menu-icon").on('click', function() {
